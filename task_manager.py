b0fa4ff40f03448ac14a06f52c872c37937891c7
GLYEB_MAG <magazgleb@gmail.com>
2024-11-06 22:02:40 +0100

    Add due_date and description

--- task_manager.py
+++ task_manager.py
@@ -1,33 +1,45 @@
+# task_manager.py
+
+from datetime import datetime
+
+
 class Task:
-    def __init__(self, name, priority, completed=False):
-        self.name = name
+    def __init__(self, title, description, priority, due_date, completed=False):
+        self.title = title
+        self.description = description
         self.priority = priority
         self.completed = completed
+        self.due_date = due_date
 
     def display(self):
         status = "Выполнено" if self.completed else "Не выполнено"
-        print(f"Задача: {self.name}, Приоритет: {self.priority}, Статус: {status}")
+        due_date_str = self.due_date.strftime("%Y-%m-%d")
+        print(f"Задача: {self.title}")
+        print(f"Описание: {self.description}")
+        print(f"Приоритет: {self.priority}")
+        print(f"Дата выполнения: {due_date_str}")
+        print(f"Статус: {status}\n")
 
 
-tasks = []  # Список задач
+tasks = []
 
 
 def add_task():
-    # Ввод данных от пользователя
-    task_name = input("Введите название задачи: ")
+    title = input("Введите название задачи: ")
+    description = input("Введите описание задачи: ")
     priority = int(
         input("Введите приоритет задачи (1 - высокий, 2 - средний, 3 - низкий): "))
+    due_date_str = input(
+        "Введите дату выполнения задачи (в формате ГГГГ-ММ-ДД): ")
+    due_date = datetime.strptime(due_date_str, "%Y-%m-%d")
     is_completed = input("Задача выполнена? (да/нет): ").lower() == "да"
 
-    # Создаём обьект задач и добавляем его в список
-    task = Task(task_name, priority, is_completed)
+    task = Task(title, description, priority, due_date, is_completed)
     tasks.append(task)
 
 
 def display_tasks():
-    # Сортировка задач по приоритету
-    tasks.sort(key=lambda x: x.priority)
-
+    tasks.sort(key=lambda x: (x.priority, x.due_date))
     print("\n--- Список задач ---")
     for task in tasks:
         task.display()
